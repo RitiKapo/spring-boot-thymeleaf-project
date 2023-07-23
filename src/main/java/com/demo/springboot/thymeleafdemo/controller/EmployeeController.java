31aedee3e10712cd47396c4f1a63dcd8077e3d27
Ritika Kapoor <ritikakapoor.imp@gmail.com>
2023-07-23 18:24:27 +0530

    Code to add an employee in the db

--- src/main/java/com/demo/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/demo/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -25,15 +27,34 @@ public class EmployeeController {
 
     // add mapping for "/list"
     @GetMapping("/list")
-    public String listEmployees(Model theModel) {
+    public String listEmployees(Model model) {
 
         // get all the employees from the database
         List<Employee> employees = employeeService.findAll();
 
         // add to the spring model
-        theModel.addAttribute("employees", employees);
+        model.addAttribute("employees", employees);
 
-        return "list-employees";
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        // create model attribute to bind the form data
+        Employee employee = new Employee();
+
+        model.addAttribute("employee", employee);
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        // save the employee
+        employeeService.save(employee);
+
+        // use a redirect to prevent duplicate submissions
+        // redirect to request mapping "/employees/list"
+        return "redirect:/employees/list";
     }
 }
 
